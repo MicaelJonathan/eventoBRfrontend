38ff3d01ed678ee472b4ba66aafbf6e403f6f3d6
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-07-03 16:30:24 -0300

    feat: definição do background da aplicação

--- evento-br-frontend/app/page.tsx
+++ evento-br-frontend/app/page.tsx
@@ -1,12 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-950 text-zinc-100 p-6 select-none">
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 select-none">
       <div className="text-center space-y-6 max-w-lg">
-        {}
-        <div className="text-6xl">🚧</div>
-        
-        {}
-        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-transparent">
+         <div className="text-6xl">🚧</div> 
+          <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-transparent">
           Em Construção...
         </h1>
       </div>
