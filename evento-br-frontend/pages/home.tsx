dffac98382d78280f5882fe659cc321e2220195a
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-07-03 17:33:14 -0300

    feat: adição do nome 'EventoBrasil'

--- evento-br-frontend/pages/home.tsx
+++ evento-br-frontend/pages/home.tsx
@@ -8,10 +8,9 @@ const font = Tilt_Warp({
 export default function HomePage({}) {
   return (
     <div className="text-center space-y-6 max-w-lg">
-      <h1 className={font.className}>EventoBrasil</h1>
-      <div className="text-6xl">🚧</div>
-      <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-transparent">
-        Em Construção...
+      <h1 className={font.className}>
+        <span className="text-blue-500 text-6xl">Evento</span>
+        <span className="text-yellow-500 text-6xl">Brasil</span>
       </h1>
     </div>
   );
