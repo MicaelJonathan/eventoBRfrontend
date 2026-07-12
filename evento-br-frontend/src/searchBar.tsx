3980c7a2c43ba69727930b150adbb1999f47aca1
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-07-12 18:30:55 -0300

    feat: adiciona estilo à combobox do site

--- evento-br-frontend/src/searchBar.tsx
+++ evento-br-frontend/src/searchBar.tsx
@@ -9,30 +9,34 @@ import {
   ComboboxItem,
 } from '@/components/ui/combobox';
 
-const examples = [
-  'Brasil',
-  'Argentina',
-  'Chile',
-  'Uruguai',
-  'Paraguai',
-  'Peru',
-  'Colômbia',
-  'Venezuela',
-  'Equador',
-  'Bolívia',
-  'Guiana',
-  'Suriname',
-];
+interface searchBarProps {
+  placeholder?: string;
+  emptyMessage?: string;
+  data?: string[];
+}
 
-export default function SearchBar() {
+export default function SearchBar({
+  placeholder = 'Search...',
+  emptyMessage = 'Item vazio',
+  data = [
+    'Item de Exemplo 1',
+    'Item de Exemplo 2',
+    'Item de Exemplo 3',
+    'Item de Exemplo 4',
+    'Item de Exemplo 5',
+  ],
+}: searchBarProps) {
   return (
-    <Combobox items={examples}>
-      <ComboboxInput placeholder="Digite o nome de um país..." />
-      <ComboboxContent>
-        <ComboboxEmpty>E esse país lá existe?</ComboboxEmpty>
+    <Combobox items={data}>
+      <ComboboxInput
+        placeholder={placeholder}
+        className={'bg-primaryui w-lg h-16 text-(--whiteui)'}
+      />
+      <ComboboxContent className={'bg-primaryui w-lg text-(--whiteui)'}>
+        <ComboboxEmpty className={'h-12 text-(--grayui)'}>{emptyMessage}</ComboboxEmpty>
         <ComboboxList>
           {(item) => (
-            <ComboboxItem key={item} value={item}>
+            <ComboboxItem key={item} value={item} className={'h-12 data-highlighted:bg-(--grayui)'}>
               {item}
             </ComboboxItem>
           )}
