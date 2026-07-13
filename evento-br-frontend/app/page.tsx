bc278159899b485892fbccf6aecd28a29b9ce300
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-07-13 15:07:27 -0300

    hotfix: remove rotas de páginas já não mais existentes na aplicação

--- evento-br-frontend/app/page.tsx
+++ evento-br-frontend/app/page.tsx
@@ -1,36 +1,9 @@
 import HomePage from '@/pages/home';
-import LoginPage from '@/pages/login';
-import CadastroPage from '@/pages/cadastro';
-import MapaPage from '@/pages/mapa';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 select-none">
       <HomePage />
-    </main>
-  );
-}
-
-export function Login() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6 select-none">
-      <LoginPage />
-    </main>
-  );
-}
-
-export function Cadastro() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6 select-none">
-      <CadastroPage />
-    </main>
-  );
-}
-
-export function Mapa() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6 select-none">
-      <MapaPage />
     </main>
   );
 }
