f4230cc9e5bb7fb6ddbe056c792d9da964372767
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-07-13 16:01:26 -0300

    feat: cria a base do card de registro de um novo usuário

--- evento-br-frontend/pages/registerUser.tsx
+++ evento-br-frontend/pages/registerUser.tsx
@@ -1,5 +1,10 @@
 import '../app/globals.css';
+import RegisterUserForm from '@/components/ui/registerUserForm';
 
 export default function RegisterUserPage({}) {
-  return <></>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-6 select-none">
+      <RegisterUserForm></RegisterUserForm>
+    </div>
+  );
 }
