8605637732746a7dad1007d04f202f6038be4415
MicaelJonathan <micaeljonatham0@gmail.com>
2026-07-27 19:44:22 -0300

    Ajustes de mapa e zoom

--- evento-br-frontend/lib/constants.ts
+++ evento-br-frontend/lib/constants.ts
@@ -4,6 +4,9 @@ interface defaultMapValuesProps {
   iconSize: [number, number];
   iconAnchor: [number, number];
   zoom: number;
+  bounds: [corner1: [number, number], corner2: [number, number], corner3: [number, number], corner4: [number, number]];
+  minZoom: number;
+  maxZoom: number;
 }
 
 export const defaultMapValues: defaultMapValuesProps = {
@@ -12,4 +15,12 @@ export const defaultMapValues: defaultMapValuesProps = {
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   zoom: 13,
+  bounds: [
+    [5.2697, -60.2097],
+    [ -33.7511, -53.3947],
+    [ -7.1550, -34.7931],
+    [-7.5531, -73.9869],
+  ],
+  minZoom: 10,
+  maxZoom: 18,
 };
