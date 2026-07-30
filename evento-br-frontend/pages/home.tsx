b91ad0c39311e084f04e90de157f7571d5729959
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-07-30 16:03:10 -0300

    feat: adiciona informações gerais no card de pesquisa

--- evento-br-frontend/pages/home.tsx
+++ evento-br-frontend/pages/home.tsx
@@ -5,6 +5,8 @@ import dynamic from 'next/dynamic';
 import AuthDialog from '@/components/auth/authDialog';
 import { defaultMapValues } from '@/lib/constants';
 import SearchBar from '@/components/ui/searchBar';
+import Image from 'next/image';
+import forro from '@/public/forro.png';
 
 const MapComponent = dynamic(() => import('@/components/ui/MapComponent'), {
   ssr: false,
@@ -27,6 +29,7 @@ export default function HomePage({}) {
       {/* Início do componente */}
       <div className="fixed top-4 left-4 z-1 rounded-md bg-neutral-100 w-1/4 h-content p-4 flex flex-col gap-4 ">
         <div className="bg-neutral-300">Nome</div>
+
         <div className="flex gap-4">
           <div className="flex-1">
             <SearchBar
@@ -35,6 +38,23 @@ export default function HomePage({}) {
             />
           </div>
           <div className="bg-white size-12 rounded-md border-1 border-gray-300"></div>
+        </div>
+
+        <div>
+          <h3>Pŕoximos Eventos</h3>
+          <ul>
+            <li className="bg-white flex items-center gap-3 border-1 border-gray-300 rounded-md">
+              <div className="relative size-24 overflow-hidden rounded-md shrink-0">
+                <Image src={forro} alt="" fill className="object-cover" />
+              </div>
+              <div className="flex flex-col">
+                <h4>Forró no Coreto</h4>
+                <span>Coreto da Praça de Edvaldo Motta</span>
+                <span>24 Mai. 20:00</span>
+                <span>250+</span>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
 
