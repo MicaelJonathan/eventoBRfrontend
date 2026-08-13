42b1ce86df6211b1019b12fdf5b83508e88125c1
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-08-13 15:30:40 -0300

    refactor: define o tipo dos eventos

--- evento-br-frontend/components/sidePanel/eventCard.tsx
+++ evento-br-frontend/components/sidePanel/eventCard.tsx
@@ -1,40 +1,41 @@
 import Image from 'next/image';
-import forro from '@/public/forro.png';
 import { Users, MapPin, Calendar, Bookmark } from 'lucide-react';
+import { EventType } from '@/types/event';
 
 export default function EventCard() {
-  const evento = {
-    imagem: forro,
-    titulo: 'Evento Patoense',
-    capacidade: 250,
-    local: 'Praça Edvaldo Motta',
-    dia: 24,
-    mes: 'maio',
-    horario: '20:00',
+  const event: EventType = {
+    id: '0',
+    title: 'Evento Patoense',
+    imageUrl: '/forro.png',
+    spot: 'Praça Edvaldo Motta',
+    date: '24 de maio',
+    hour: '20:00',
+    participantsQuantity: 250,
+    saved: false,
   };
 
   return (
     <div className="bg-white flex w-full items-center border-1 border-gray-300 rounded-md">
       <div className="relative size-24 overflow-hidden rounded-md shrink-0">
-        <Image src={evento.imagem} alt="" fill className="object-cover" />
+        <Image src={event.imageUrl} alt="" fill className="object-cover" />
       </div>
 
       <div className="flex flex-1 items-center">
         <div className="flex flex-col p-2">
-          <h4>{evento.titulo}</h4>
+          <h4>{event.title}</h4>
           <ul className="flex flex-col gap-1">
             <li className="flex text-gray-500 items-center gap-1">
               <Users className="size-4" />
-              <p className="text-xs">{evento.capacidade}+</p>
+              <p className="text-xs">{event.participantsQuantity}+</p>
             </li>
             <li className="flex text-gray-500 items-center gap-1">
               <MapPin className="size-4" />
-              <p className="text-xs">{evento.local}</p>
+              <p className="text-xs">{event.spot}</p>
             </li>
             <li className="flex text-gray-500 items-center gap-1">
               <Calendar className="size-4" />
               <p className="text-xs">
-                {evento.dia} de {evento.mes} às {evento.horario}
+                {event.date} às {event.hour}
               </p>
             </li>
           </ul>
