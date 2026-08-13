4e14373ab9c13770974569693dec5c7b6cedbbf9
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-08-13 15:14:35 -0300

    refactor: eventCard utiliza um objeto e não mais nomes estáticos para mostrar dados

--- evento-br-frontend/components/sidePanel/eventCard.tsx
+++ evento-br-frontend/components/sidePanel/eventCard.tsx
@@ -3,27 +3,39 @@ import forro from '@/public/forro.png';
 import { Users, MapPin, Calendar, Bookmark } from 'lucide-react';
 
 export default function EventCard() {
+  const evento = {
+    imagem: forro,
+    titulo: 'Evento Patoense',
+    capacidade: 250,
+    local: 'Praça Edvaldo Motta',
+    dia: 24,
+    mes: 'maio',
+    horario: '20:00',
+  };
+
   return (
     <div className="bg-white flex w-full items-center border-1 border-gray-300 rounded-md">
       <div className="relative size-24 overflow-hidden rounded-md shrink-0">
-        <Image src={forro} alt="" fill className="object-cover" />
+        <Image src={evento.imagem} alt="" fill className="object-cover" />
       </div>
 
       <div className="flex flex-1 items-center">
         <div className="flex flex-col p-2">
-          <h4>Forró no Coreto</h4>
+          <h4>{evento.titulo}</h4>
           <ul className="flex flex-col gap-1">
             <li className="flex text-gray-500 items-center gap-1">
               <Users className="size-4" />
-              <p className="text-xs">250+</p>
+              <p className="text-xs">{evento.capacidade}+</p>
             </li>
             <li className="flex text-gray-500 items-center gap-1">
               <MapPin className="size-4" />
-              <p className="text-xs">Praça Edvaldo Motta</p>
+              <p className="text-xs">{evento.local}</p>
             </li>
             <li className="flex text-gray-500 items-center gap-1">
               <Calendar className="size-4" />
-              <p className="text-xs">24 Mai às 20:00</p>
+              <p className="text-xs">
+                {evento.dia} de {evento.mes} às {evento.horario}
+              </p>
             </li>
           </ul>
         </div>
--- evento-br-frontend/components/sidePanel/sidePanel.tsx
+++ evento-br-frontend/components/sidePanel/sidePanel.tsx
@@ -22,6 +22,7 @@ export default function SidePanel() {
 
   useEffect(() => {
     const token = localStorage.getItem('token');
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsLoggedIn(!!token);
   }, []);
 
