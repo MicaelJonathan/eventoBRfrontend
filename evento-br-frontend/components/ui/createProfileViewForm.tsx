4f06b1adf1975f9b128129c25bb44d49fff19d88
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-08-14 10:58:57 -0300

    feat: enfeita a tela de perfil de usuário com frufruzinhos

--- evento-br-frontend/components/ui/createProfileViewForm.tsx
+++ evento-br-frontend/components/ui/createProfileViewForm.tsx
@@ -10,29 +10,34 @@ export default function ProfileView() {
   const router = useRouter();
 
   return (
-    <Card size="default" className="mx-auto w-full max-w-xl max-h-fit shadow-md">
-      <CardHeader>
-        <span className="font-semibold text-center text-xl">Perfil do Usuário</span>
-      </CardHeader>
-      <CardContent className="flex flex-col items-center">
-        <User className="size-50 bg-gray-200 rounded-full p-4 " />
-        <span className="text-lg mt-4">Em construção...</span>
-      </CardContent>
-      <CardFooter className="flex text-white gap-2 justify-center">
-        <Button
-          className={'h-12 px-6 rounded-2xl bg-purple-700 text-sm font-bold hover:bg-purple-900'}
-          onClick={() => {
-            router.push('/home');
-          }}
-        >
-          <span className="text-sm font-medium font-semibold">Voltar para home</span>
-        </Button>
-        <LogOut
-          onSuccess={() => {
-            router.push('/home');
-          }}
-        />
-      </CardFooter>
-    </Card>
+    <div className="bg-purple-100 size-full flex min-h-screen flex-col items-center justify-center p-6 select-none z-[-1]">
+      <div className="size-120 bg-linear-to-r from-purple-300 to-purple-400 fixed top-50 left-40 rounded-full blur-3xl z-[-0]"></div>
+      <div className="size-120 bg-radial from-purple-400 to-purple-300 fixed top-0 right-[-200] rounded-full blur-3xl z-[-0]"></div>
+      <div className="size-200 bg-linear-to-r from-purple-300 to-purple-400 fixed bottom-[-400] right-70 rounded-full blur-3xl z-[-0]"></div>
+      <Card size="default" className="mx-auto w-4xl max-w-xl max-h-fit shadow-md z-1 bg-purple-100">
+        <CardHeader>
+          <span className="font-semibold text-center text-xl">Perfil do Usuário</span>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center">
+          <User className="size-50 bg-white rounded-full p-4 " />
+          <span className="text-lg mt-4">Em construção...</span>
+        </CardContent>
+        <CardFooter className="flex text-white gap-2 justify-center">
+          <Button
+            className={'h-12 px-6 rounded-2xl bg-purple-700 text-sm font-bold hover:bg-purple-900'}
+            onClick={() => {
+              router.push('/home');
+            }}
+          >
+            <span className="text-sm font-medium font-semibold">Voltar para home</span>
+          </Button>
+          <LogOut
+            onSuccess={() => {
+              router.push('/home');
+            }}
+          />
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
