fa24865460fe6d534ed809b3293f6caa802e960a
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-08-14 10:38:35 -0300

    feat: constrói o estilo base da tela de perfil do usuário

--- evento-br-frontend/components/ui/createProfileViewForm.tsx
+++ evento-br-frontend/components/ui/createProfileViewForm.tsx
@@ -1,42 +1,38 @@
 'use client';
 
+import { User } from 'lucide-react';
 import { Button } from '@base-ui/react';
 import { useRouter } from 'next/navigation';
-import { Card, CardHeader, CardContent } from './card';
-import Link from 'next/link';
+import { Card, CardHeader, CardContent, CardFooter } from './card';
 import LogOut from '@/components/auth/logOut';
 
 export default function ProfileView() {
   const router = useRouter();
 
   return (
-    <Card size="default" className="mx-auto w-full max-w-xl max-h-fit">
+    <Card size="default" className="mx-auto w-full max-w-xl max-h-fit shadow-md">
       <CardHeader>
-        <h3 className="font-semibold">Perfil do usuário</h3>
+        <span className="font-semibold text-center text-xl">Perfil do Usuário</span>
       </CardHeader>
-      <CardContent>
-        <div className="space-y-4">
-          <div>
-            <p>Nome: </p>
-          </div>
-          <div>
-            <p>Email: </p>
-          </div>
-        </div>
+      <CardContent className="flex flex-col items-center">
+        <User className="size-50 bg-gray-200 rounded-full p-4 " />
+        <span className="text-lg mt-4">Em construção...</span>
       </CardContent>
-      <Button>
-        <span className="text-sm font-medium">Deletar perfil</span>
-      </Button>
-      <Button>
-        <Link href="/home">
-          <span className="text-sm font-medium">Voltar para home</span>
-        </Link>
-      </Button>
-      <LogOut
-        onSuccess={() => {
-          router.push('/home');
-        }}
-      />
+      <CardFooter className="flex text-white gap-2 justify-center">
+        <Button
+          className={'h-12 px-6 rounded-2xl bg-purple-700 text-sm font-bold hover:bg-purple-900'}
+          onClick={() => {
+            router.push('/home');
+          }}
+        >
+          <span className="text-sm font-medium font-semibold">Voltar para home</span>
+        </Button>
+        <LogOut
+          onSuccess={() => {
+            router.push('/home');
+          }}
+        />
+      </CardFooter>
     </Card>
   );
 }
