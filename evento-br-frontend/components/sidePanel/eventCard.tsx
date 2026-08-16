60836fb851a0a9c89678a620583cbf435da8dd8a
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-08-16 20:04:21 -0300

    feat: adiciona os eventos na barra lateral e ainda corrige os estilos necessários

--- evento-br-frontend/components/sidePanel/eventCard.tsx
+++ evento-br-frontend/components/sidePanel/eventCard.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
 import { Users, MapPin, Calendar, Bookmark } from 'lucide-react';
-import { EventType } from '@/types/event';
+import { getEventsResponseType } from '@/types/getEventsResponse';
+import { Image } from 'lucide-react';
 
 interface EventCardProps {
-  event: EventType;
+  event: getEventsResponseType;
 }
 
 export default function EventCard({ event }: EventCardProps) {
@@ -13,26 +13,24 @@ export default function EventCard({ event }: EventCardProps) {
   return (
     <div className="bg-white flex w-full items-center border-1 border-gray-300 rounded-md">
       <div className="relative size-24 overflow-hidden rounded-md shrink-0">
-        <Image src={event.imageUrl} alt="" fill className="object-cover" />
+        <Image className="size-full" />
       </div>
 
       <div className="flex flex-1 items-center">
         <div className="flex flex-col p-2">
-          <h4>{event.title}</h4>
+          <h4>{event.name}</h4>
           <ul className="flex flex-col gap-1">
             <li className={informationLineClassName}>
               <Users className={iconsSizeClassName} />
-              <span>{event.participantsQuantity}+</span>
+              <span>{event.capacity}+</span>
             </li>
             <li className={informationLineClassName}>
               <MapPin className={iconsSizeClassName} />
-              <span>{event.spot}</span>
+              <span>{event.location}</span>
             </li>
             <li className={informationLineClassName}>
               <Calendar className={iconsSizeClassName} />
-              <span>
-                {event.date} às {event.hour}
-              </span>
+              <span>{event.date_Time}</span>
             </li>
           </ul>
         </div>
--- evento-br-frontend/components/sidePanel/eventsList.tsx
+++ evento-br-frontend/components/sidePanel/eventsList.tsx
@@ -1,16 +1,16 @@
-import type { EventType } from '@/types/event';
+import { getEventsResponseType } from '@/types/getEventsResponse';
 import EventCard from './eventCard';
 
 interface EventsListProps {
-  events: EventType[];
+  events: getEventsResponseType[] | null;
 }
 
 export default function EventsList({ events }: EventsListProps) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex min-h-0 flex-1 flex-col gap-2 overflow-y-auto pr-1">
       <h3 className="text-lg font-semibold">Pŕoximos Eventos</h3>
       <ul className="flex flex-col gap-2">
-        {events.map((event) => (
+        {events?.map((event) => (
           <li key={event.id}>
             <EventCard event={event} />
           </li>
