4d1d0c30eb264d18627c925f85b30bf83e4f81e7
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-08-16 11:06:06 -0300

    feat: implementa o hook de cadastro de eventos

--- evento-br-frontend/hooks/useCreateEvent.ts
+++ evento-br-frontend/hooks/useCreateEvent.ts
@@ -1,7 +1,15 @@
-import type { CreateEventSchemaType } from '@/schemas/createEventSchema';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 import { createEvent } from '@/services/eventService';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CreateEventSchemaType, CreateEventSchema } from '@/schemas/createEventSchema';
 
 export function useCreateEvent() {
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<CreateEventSchemaType>({ resolver: zodResolver(CreateEventSchema) });
+
   const paylod: CreateEventSchemaType = {
     name: 'string',
     description: 'string',
@@ -13,8 +21,18 @@ export function useCreateEvent() {
   };
 
   const handleCreateEvent = async () => {
-    await createEvent(paylod);
+    setServerError(null);
+    setIsSubmitting(true);
+
+    try {
+      await createEvent(paylod);
+    } catch (e) {
+      setServerError('Informações do formulário não preenchidas corretamente');
+      console.error(e);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
-  return { handleCreateEvent };
+  return { ...form, handleCreateEvent, serverError, isSubmitting };
 }
