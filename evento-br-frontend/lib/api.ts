0299c927d3c7f45a1a8a3253b78c4db4e8b60fff
Kayke Nícolas <kaykenicolasprofissional@gmail.com>
2026-08-16 10:20:34 -0300

    refactor: passa o token de acesso dentro da api

--- evento-br-frontend/lib/api.ts
+++ evento-br-frontend/lib/api.ts
@@ -1,3 +1,9 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_API_URL });
+
+api.interceptors.request.use((request: InternalAxiosRequestConfig) => {
+  const token = localStorage.getItem('token');
+  if (token) request.headers.Authorization = `Bearer ${token}`;
+  return request;
+});
